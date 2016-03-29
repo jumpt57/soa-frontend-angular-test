0548c1cc5fe94ae8798c179376cd23ad1a1fafe5
Jessica Lacaze <lacaze.jessica@gmail.com>
2016-03-29 16:01:13 +0200

    interface d'affichage des items

--- js/item.ctrl.js
+++ js/item.ctrl.js
@@ -1,5 +1,37 @@
 'use strict';
 
-angular.module('catalogueApp').controller('ItemCtrl', function ($scope, $location, $http, $routeParams) {
+angular.module('catalogueApp').controller('ItemCtrl', ['$scope', '$routeParams', '$http', '$location', function($scope, $routeParams, $http, $location){
+	
+    document.getElementById("category").className="";
+    document.getElementById("item").className="active";
+    document.getElementById("search").className="";
 
-});
+    if($routeParams.category){
+		$scope.items = [];
+        $http({
+            method: 'GET',
+            url: './json/items.json'
+        }).then(function successCallback(response) {
+            angular.forEach(response.data, function (item) {
+            	if(item.categ==$routeParams.category)
+            		$scope.items.push(item);
+            });
+        }, function errorCallback(response) {
+            $scope.message = JSON.stringify(response);
+        });
+	}
+	else{
+		$http({
+            method: 'GET',
+            url: './json/items.json'
+        }).then(function successCallback(response) {
+            $scope.items = response.data;
+        }, function errorCallback(response) {
+            $scope.message = JSON.stringify(response);
+        });
+	}
+
+    $scope.voir = function(item){
+        $location.path('/itemDetail/'+item.id);
+    };
+}]);
