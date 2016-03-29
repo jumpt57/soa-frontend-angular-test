024d972b3aafb9bc466dbdcaf3ca840981fb771f
Jessica Lacaze <lacaze.jessica@gmail.com>
2016-03-29 15:59:15 +0200

    interface permettant de sélectionner les items par categories

--- js/category.ctrl.js
+++ js/category.ctrl.js
@@ -1,5 +1,21 @@
 'use strict';
 
-angular.module('catalogueApp').controller('CategoryCtrl', function ($scope, $location, $http, $routeParams) {
+angular.module('catalogueApp').controller('CategoryCtrl', ['$scope', '$routeParams', '$http', '$location', function($scope, $routeParams, $http, $location){
+	
+    document.getElementById("category").className="active";
+    document.getElementById("item").className="";
+    document.getElementById("search").className="";
 
-});
+	$http({
+        method: 'GET',
+        url: './json/categories.json'
+    }).then(function successCallback(response) {
+    	$scope.categories = response.data;
+    }, function errorCallback(response) {
+        $scope.message = JSON.stringify(response);
+    });
+
+    $scope.categoryClick = function(category){
+        $location.path('/item/'+category.id);
+    };
+}]);
